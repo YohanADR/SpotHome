8079da03890521fa228d1b5075c9c6fb02be325d
YohanADR <yohan.andre@outlook.com>
2024-09-21 18:15:28 +0200

    add database connection

--- cmd/main.go
+++ cmd/main.go
@@ -4,18 +4,29 @@ import (
 	"SpotHome/internal/infrastructure/config"
 	"SpotHome/internal/infrastructure/http_server"
 	"SpotHome/internal/infrastructure/logger"
+	"SpotHome/internal/infrastructure/postgis"
 	"SpotHome/internal/infrastructure/router"
+	"fmt"
 )
 
 func main() {
 
-	cfg := config.Load()      // Charge la configuration
 	log := logger.NewLogger() // Retourne l'interface logger
+	cfg := config.Load(log)   // Charge la configuration
 	r := router.NewRouter()   // Crée une nouvelle instance de router
+
+	connString := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%d sslmode=%s",
+		cfg.DB_USER, cfg.DB_PASSWORD, cfg.DB_NAME, cfg.DB_HOST, cfg.DB_PORT, cfg.DB_SSLMODE)
+	pg, err := postgis.NewPostGIS(connString) // Crée une nouvelle instance de PostGIS
+
+	if err != nil {
+		log.Error("Échec de la connexion à la base de données PostGIS: %v", logger.FieldError(err))
+	}
+	defer pg.Close()
 
 	defer log.Sync()   // assure d'écrire les logs avant de quitter
 	r.RegisterRoutes() // Enregistre les routes
 
 	log.Info("Démarrage de l'application en mode", logger.FieldString("Environment", cfg.Environment))
-	http_server.StartServer(cfg, r, log)
+	http_server.StartServer(cfg, r, log) // Démarrage du serveur HTTP
 }
--- internal/infrastructure/postgis/postgis.go
+++ internal/infrastructure/postgis/postgis.go
@@ -1,1 +1,28 @@
+// postgis/postgis.go
 package postgis
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+// PostGIS est une structure pour gérer la connexion à la base de données PostGIS
+type PostGIS struct {
+	pool *pgxpool.Pool
+}
+
+// NewPostGIS crée une nouvelle instance de PostGIS avec une connexion à la base de données
+func NewPostGIS(connString string) (*PostGIS, error) {
+	pool, err := pgxpool.Connect(context.Background(), connString)
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to database: %v", err)
+	}
+	return &PostGIS{pool: pool}, nil
+}
+
+// Close ferme la connexion à la base de données
+func (p *PostGIS) Close() {
+	p.pool.Close()
+}
